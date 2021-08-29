cfd5e8fbc775b9bd251c857fe5037fa73cca44bc
Anthony Fontanez <ajf@anthonyfontanez.com>
2021-08-29 18:32:18 -0400

    Updated New-LabOUStructure.ps1, added New-LabUserAccounts.ps1

--- Build/New-LabOUStructure.ps1
+++ Build/New-LabOUStructure.ps1
@@ -1,6 +1,7 @@
 Import-Module -Name ActiveDirectory
 
 New-ADOrganizationalUnit -Name "LAB" -Path "DC=lab,DC=ajf8729,DC=com" -Description "LAB Root OU"
+New-ADOrganizationalUnit -Name "T0" -Path "DC=lab,DC=ajf8729,DC=com" -Description "Tier 0 Objects"
 
 $OUs = (
     "Administrators",
