f12dc4a4a19471071a9252bc36ffc5e9771e3591
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-11-01 04:45:51 -0400

    Bug fixes and improvements

--- 03a-SecurityBaselines.ps1
+++ 03a-SecurityBaselines.ps1
@@ -1,20 +1,48 @@
-New-GPLink -Name "MSFT Windows Server 2022 - Domain Controller" -Target "OU=Domain Controllers,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 2 | Out-Null
-New-GPLink -Name "MSFT Windows Server 2022 - Defender Antivirus" -Target "OU=Domain Controllers,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 3 | Out-Null
+# Modules
 
-New-GPLink -Name "MSFT Windows Server 2022 - Member Server" -Target "OU=Servers,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 2 | Out-Null
-New-GPLink -Name "MSFT Windows Server 2022 - Defender Antivirus" -Target "OU=Servers,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 3 | Out-Null
+Import-Module -Name ActiveDirectory
 
-New-GPLink -Name "MSFT Windows 10 21H1 - Computer" -Target "OU=Autopilot,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 3 | Out-Null
-New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus" -Target "OU=Autopilot,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 4 | Out-Null
+# Variables
 
-New-GPLink -Name "MSFT Windows 10 21H1 - Computer" -Target "OU=Staging,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 3 | Out-Null
-New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus" -Target "OU=Staging,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 4 | Out-Null
+$BaseURL = "https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8"
 
-New-GPLink -Name "MSFT Windows 10 21H1 - Computer" -Target "OU=Workstations,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 2 | Out-Null
-New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus" -Target "OU=Workstations,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 3 | Out-Null
+$DomainDistinguishedName = (Get-ADDomain).DistinguishedName
+$DomainNetBIOSName       = (Get-ADDomain).NetBIOSName
+$RootOUDistinguishedName = "OU=$($DomainNetBIOSName),$($DomainDistinguishedName)"
 
-New-GPLink -Name "MSFT Windows 10 21H1 - User" -Target "OU=T0,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 1 | Out-Null
+# Download baselines
 
-New-GPLink -Name "MSFT Windows 10 21H1 - User" -Target "OU=Administrators,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 1 | Out-Null
+Invoke-WebRequest -UseBasicParsing -Uri "$($BaseURL)/Windows%2011%20Security%20Baseline.zip"                   -OutFile "Windows11SecurityBaseline.zip"
+Invoke-WebRequest -UseBasicParsing -Uri "$($BaseURL)/Windows%20Server%202022%20Security%20Baseline.zip"        -OutFile "WindowsServer2022SecurityBaseline.zip"
+Invoke-WebRequest -UseBasicParsing -Uri "$($BaseURL)/Microsoft%20365%20Apps%20for%20Enterprise-2104-FINAL.zip" -OutFile "Microsoft365AppsforEnterprise-2104-FINAL.zip"
+Invoke-WebRequest -UseBasicParsing -Uri "$($BaseURL)/Microsoft%20Edge%20v95%20Security%20Baseline.zip"         -OutFile "MicrosoftEdgev95SecurityBaseline.zip"
 
-New-GPLink -Name "MSFT Windows 10 21H1 - User" -Target "OU=Users,OU=LAB,DC=lab,DC=ajf8729,DC=com" -LinkEnabled Yes -Enforced No -Order 2 | Out-Null
+# Extract baselines
+
+(Get-ChildItem -Path "*.zip").Name | ForEach-Object {
+    Expand-Archive -Path $_ -DestinationPath .
+}
+
+# Import baselines
+
+(Get-ChildItem -Directory).Name | ForEach-Object {
+    .$_\Scripts\Baseline-ADImport.ps1
+}
+
+# Link GPOs
+
+New-GPLink -Name "MSFT Windows Server 2022 - Domain Controller"  -Target "OU=Domain Controllers,$DomainDistinguishedName" -LinkEnabled Yes -Enforced No -Order 2
+New-GPLink -Name "MSFT Windows Server 2022 - Defender Antivirus" -Target "OU=Domain Controllers,$DomainDistinguishedName" -LinkEnabled Yes -Enforced No -Order 3
+New-GPLink -Name "MSFT Windows 10 21H1 - User"                   -Target "OU=T0,$DomainDistinguishedName"                 -LinkEnabled Yes -Enforced No -Order 1
+New-GPLink -Name "MSFT Windows 10 21H1 - User"                   -Target "OU=Administrators,$RootOUDistinguishedName"     -LinkEnabled Yes -Enforced No -Order 1
+New-GPLink -Name "MSFT Windows 10 21H1 - Computer"               -Target "OU=Autopilot,$RootOUDistinguishedName"          -LinkEnabled Yes -Enforced No -Order 3
+New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus"     -Target "OU=Autopilot,$RootOUDistinguishedName"          -LinkEnabled Yes -Enforced No -Order 4
+New-GPLink -Name "MSFT Windows 10 21H1 - Computer"               -Target "OU=Kiosks,$RootOUDistinguishedName"             -LinkEnabled Yes -Enforced No -Order 3
+New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus"     -Target "OU=Kiosks,$RootOUDistinguishedName"             -LinkEnabled Yes -Enforced No -Order 4
+New-GPLink -Name "MSFT Windows Server 2022 - Member Server"      -Target "OU=Servers,$RootOUDistinguishedName"            -LinkEnabled Yes -Enforced No -Order 2
+New-GPLink -Name "MSFT Windows Server 2022 - Defender Antivirus" -Target "OU=Servers,$RootOUDistinguishedName"            -LinkEnabled Yes -Enforced No -Order 3
+New-GPLink -Name "MSFT Windows 10 21H1 - Computer"               -Target "OU=Staging,$RootOUDistinguishedName"            -LinkEnabled Yes -Enforced No -Order 3
+New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus"     -Target "OU=Staging,$RootOUDistinguishedName"            -LinkEnabled Yes -Enforced No -Order 4
+New-GPLink -Name "MSFT Windows 10 21H1 - User"                   -Target "OU=Users,$RootOUDistinguishedName"              -LinkEnabled Yes -Enforced No -Order 2
+New-GPLink -Name "MSFT Windows 10 21H1 - Computer"               -Target "OU=Workstations,$RootOUDistinguishedName"       -LinkEnabled Yes -Enforced No -Order 2
+New-GPLink -Name "MSFT Windows 10 21H1 - Defender Antivirus"     -Target "OU=Workstations,$RootOUDistinguishedName"       -LinkEnabled Yes -Enforced No -Order 3
