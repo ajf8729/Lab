bb014e19d4472d7b96e01f1c2203bf5e12680e81
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-11-02 08:38:51 -0400

    Updated hostname

--- 04-ConfigureCM.ps1
+++ 04-ConfigureCM.ps1
@@ -11,23 +11,23 @@ Get-CMSiteFeature -Fast | Where-Object -FilterScript {$_.Status -eq "0"} | Enabl
 Set-CMDiscoveryMethod -ActiveDirectoryForestDiscovery -Enabled $true -EnableActiveDirectorySiteBoundaryCreation $true -PollingSchedule (New-CMSchedule -RecurCount 7 -RecurInterval Days)
 Invoke-CMForestDiscovery
 
-Set-CMDiscoveryMethod -ActiveDirectoryGroupDiscovery -Enabled $true -AddGroupDiscoveryScope (New-CMADGroupDiscoveryScope -LdapLocation "LDAP://DC=lab,DC=ajf8729,DC=com" -Name "lab.ajf8729.com" -RecursiveSearch $true) -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days) -EnableFilteringExpiredLogon $true -EnableFilteringExpiredPassword $true
+Set-CMDiscoveryMethod -ActiveDirectoryGroupDiscovery -Enabled $true -AddGroupDiscoveryScope (New-CMADGroupDiscoveryScope -LdapLocation "LDAP://DC=lab,DC=dev,DC=ajf8729,DC=com" -Name "lab.dev.ajf8729.com" -RecursiveSearch $true) -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days) -EnableFilteringExpiredLogon $true -EnableFilteringExpiredPassword $true
 Invoke-CMGroupDiscovery
 
-Set-CMDiscoveryMethod -ActiveDirectorySystemDiscovery -Enabled $true -AddActiveDirectoryContainer "LDAP://DC=lab,DC=ajf8729,DC=com" -EnableIncludeGroup $true -EnableRecursive $true -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days) -EnableFilteringExpiredLogon $true -EnableFilteringExpiredPassword $true
+Set-CMDiscoveryMethod -ActiveDirectorySystemDiscovery -Enabled $true -AddActiveDirectoryContainer "LDAP://DC=lab,DC=dev,DC=ajf8729,DC=com" -EnableIncludeGroup $true -EnableRecursive $true -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days) -EnableFilteringExpiredLogon $true -EnableFilteringExpiredPassword $true
 Invoke-CMSystemDiscovery
 
-Set-CMDiscoveryMethod -ActiveDirectoryUserDiscovery -Enabled $true -AddActiveDirectoryContainer "LDAP://DC=lab,DC=ajf8729,DC=com" -EnableIncludeGroup $true -EnableRecursive $true -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days)
+Set-CMDiscoveryMethod -ActiveDirectoryUserDiscovery -Enabled $true -AddActiveDirectoryContainer "LDAP://DC=lab,DC=dev,DC=ajf8729,DC=com" -EnableIncludeGroup $true -EnableRecursive $true -PollingSchedule (New-CMSchedule -RecurCount 1 -RecurInterval Days)
 Invoke-CMUserDiscovery
 
 # Configure boundary & boundary group
 
-New-CMBoundaryGroup -Name "LAB" -DefaultSiteCode "LAB" -AddSiteSystemServerName "labcm01.lab.ajf8729.com" | Out-Null
-Add-CMBoundaryToGroup -BoundaryName "lab.ajf8729.com/LAB" -BoundaryGroupName "LAB"
+New-CMBoundaryGroup -Name "LAB" -DefaultSiteCode "LAB" -AddSiteSystemServerName "labcm01.lab.dev.ajf8729.com" | Out-Null
+Add-CMBoundaryToGroup -BoundaryName "lab.dev.ajf8729.com/LAB" -BoundaryGroupName "LAB"
 
 # Configure default boundary group
 
-Invoke-CimMethod -InputObject (Get-CimInstance -Namespace "root\sms\site_LAB" -ClassName SMS_DefaultBoundaryGroup) -MethodName AddSiteSystem -Arguments @{ServerNALPath = [string[]]'["Display=\\LABCM01.lab.ajf8729.com\"]MSWNET:["SMS_SITE=LAB"]\\LABCM01.lab.ajf8729.com\'; Flags=([System.UInt32[]]0)}
+Invoke-CimMethod -InputObject (Get-CimInstance -Namespace "root\sms\site_LAB" -ClassName SMS_DefaultBoundaryGroup) -MethodName AddSiteSystem -Arguments @{ServerNALPath = [string[]]'["Display=\\LABCM01.lab.dev.ajf8729.com\"]MSWNET:["SMS_SITE=LAB"]\\LABCM01.lab.dev.ajf8729.com\'; Flags=([System.UInt32[]]0)}
 
 # Site properties
 
@@ -41,4 +41,4 @@ Set-CMHierarchySetting -EnableAutoClientUpgrade $true -AutomaticallyUpgradeDays 
 # DP Group
 
 New-CMDistributionPointGroup -Name "ALL" | Out-Null
-Add-CMDistributionPointToGroup -DistributionPointGroupName "ALL" -DistributionPointName "labcm01.lab.ajf8729.com"
+Add-CMDistributionPointToGroup -DistributionPointGroupName "ALL" -DistributionPointName "labcm01.lab.dev.ajf8729.com"
