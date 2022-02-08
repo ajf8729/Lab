b32685673154a50908e1a2c2ce2bfac9c1fc7c99
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2022-02-07 22:07:20 -0500

    Added folder creations, removed Out-Null's

--- ConfigureConfigMgrServer.ps1
+++ ConfigureConfigMgrServer.ps1
@@ -5,9 +5,11 @@ Param(
 # Configure disks
 Get-Disk | Where-Object {$_.OperationalStatus -eq 'Offline'} | Set-Disk -IsOffline $false
 Get-Disk | Where-Object {$_.IsReadOnly -eq $true} | Initialize-Disk -PartitionStyle GPT
-New-Volume -DiskNumber 1 -FileSystem NTFS -DriveLetter 'E' -FriendlyName 'SQL' | Out-Null
-New-Volume -DiskNumber 2 -FileSystem NTFS -DriveLetter 'F' -FriendlyName 'CM' | Out-Null
-New-Item -Path 'F:\' -Name 'SOURCE' -ItemType Directory | Out-Null
+New-Volume -DiskNumber 1 -FileSystem NTFS -DriveLetter 'E' -FriendlyName 'SQL'
+New-Volume -DiskNumber 2 -FileSystem NTFS -DriveLetter 'F' -FriendlyName 'CM'
+New-Item -Path 'F:\' -Name 'SOURCE' -ItemType Directory
+New-Item -Path 'F:\' -Name 'WSUS' -ItemType Directory
+New-Item -Path 'F:\SOURCE\' -Name 'Downloads' -ItemType Directory
 
 # Install RSAT tools
 Install-WindowsFeature -Name RSAT-AD-PowerShell, RSAT-AD-AdminCenter, RSAT-ADDS-Tools, GPMC
@@ -15,3 +17,7 @@ Install-WindowsFeature -Name RSAT-AD-PowerShell, RSAT-AD-AdminCenter, RSAT-ADDS-
 # Install gMSA for SQL
 Install-ADServiceAccount -Identity 'svc_CM_SQL'
 Restart-Computer
+
+# Create NO_SMS_ON_DRIVE.SMS files
+New-Item -Path 'C:\' -Name "NO_SMS_ON_DRIVE.SMS" -ItemType File
+New-Item -Path 'E:\' -Name "NO_SMS_ON_DRIVE.SMS" -ItemType File
